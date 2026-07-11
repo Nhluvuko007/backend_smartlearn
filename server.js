0e51d93618549dddf2bb54f4068c1c7e4874abad
Nhluvuko007 <nhluvukoremember@gmail.com>
2026-07-11 04:32:31 +0200

    New info added

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://smartlearn-bice.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json()); // Essential for handling incoming JSON data
 
 // Establish MongoDB Connection
