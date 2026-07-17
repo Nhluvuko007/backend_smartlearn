aaf0c90f2c5187bfcf99f7cf3fb93951770a3958
Nhluvuko007 <nhluvukoremember@gmail.com>
2026-07-17 14:33:17 +0200

    updated files

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: 'https://smartlearn-bice.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
 
